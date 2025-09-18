c86bd939ce27513210c1f4cd79e6ab53b062dd97
Sujin14 <sujinsuresh1422002@gmail.com>
2025-09-18 14:30:35 +0530

    added new version number in settings

--- lib/features/settings/presentation/widgets/settings_body.dart
+++ lib/features/settings/presentation/widgets/settings_body.dart
@@ -87,7 +87,7 @@ class SettingsBody extends ConsumerWidget {
           SizedBox(height: 24.h),
           Center(
             child: Text(
-              'App Version: 1.0.0',
+              'App Version: 1.0.1',
               style: AppTheme.textStyles['body']!.copyWith(color: AppTheme.colors['secondaryText']),
             ),
           ),
